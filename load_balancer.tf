2c9e346621ccbd550b29f6447b03f0a0299f6bcf
jamayapabon <jeap1723@gmail.com>
2024-07-04 23:52:17 -0500

    Added listener

--- load_balancer.tf
+++ load_balancer.tf
@@ -30,6 +30,17 @@ resource "aws_lb_target_group_attachment" "test2" {
 #   port             = 80
 # }
 
+resource "aws_lb_listener" "lb_listener" {
+  load_balancer_arn = aws_lb.load_balancer.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.target_group.arn
+  }
+}
+
 resource "aws_lb" "load_balancer" {
   name               = "my-load-balancer"
   internal           = false
